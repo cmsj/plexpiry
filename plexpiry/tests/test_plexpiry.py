31ad89081bd3d33a663320b74900de41b5f7a977
Chris Jones <cmsj@tenshu.net>
2013-11-12 15:40:12 +0800

    Continue fleshing out XML tests

--- plexpiry/tests/test_plexpiry.py
+++ plexpiry/tests/test_plexpiry.py
@@ -1,5 +1,7 @@
 import fixtures
+import os
 import testtools
+import urlparse
 
 from plexpiry import plexpiry
 
@@ -15,6 +17,15 @@ FAKE_SECTIONS = {'show': 3,
                  'movies': 5,
                  }
 
+FAKE_PATH = os.path.dirname(__file__)
+FAKE_SECTIONS_XML = os.path.join(FAKE_PATH, "fake_sections.xml")
+FAKE_TV_SHOWS = os.path.join(FAKE_PATH, "fake_tv_shows.xml")
+FAKE_TV_SEASONS = os.path.join(FAKE_PATH, "fake_tv_seasons.xml")
+FAKE_TV_EPISODES = os.path.join(FAKE_PATH, "fake_tv_episodes.xml")
+FAKE_TV_EPISODE = os.path.join(FAKE_PATH, "fake_tv_episode.xml")
+FAKE_MOVIES = os.path.join(FAKE_PATH, "fake_movies.xml")
+FAKE_MOVIE = os.path.join(FAKE_PATH, "fake_movie.xml")
+
 
 class FakeOptions():
     debug = False
@@ -26,12 +37,20 @@ class TestPlexpiry(testtools.TestCase):
 
     def setUp(self):
         super(TestPlexpiry, self).setUp()
+
+        def fake_urlopen(url):
+            url = urlparse.urlparse(url)
+            if url.path == "/library/sections":
+                return open(FAKE_SECTIONS_XML)
+            raise ValueError("Unknown request: %s" % url.path)
+
         self.useFixture(fixtures.NestedTempfile())
         self.stdout = self.useFixture(fixtures.StringStream('stdout')).stream
         self.useFixture(fixtures.MonkeyPatch('sys.stdout', self.stdout))
         stderr = self.useFixture(fixtures.StringStream('stderr')).stream
         self.useFixture(fixtures.MonkeyPatch('sys.stderr', stderr))
         self.logger = self.useFixture(fixtures.FakeLogger(name="plexpiry"))
+        self.useFixture(fixtures.MonkeyPatch('urllib2.urlopen', fake_urlopen))
         self.addCleanup(self.cleanUp)
 
         self.options = FakeOptions()
@@ -89,3 +108,27 @@ class TestPlexpiry(testtools.TestCase):
     def test_find_sections(self):
         self.plexpiry.find_sections()
         self.assertEqual(FAKE_SECTIONS, self.plexpiry.sections)
+
+    def test_find_tv_shows(self):
+        shows = self.plexpiry.find_tv_shows()
+        self.assertEqual(FAKE_TV_SHOWS, shows)
+
+    def test_find_tv_seasons(self):
+        seasons = self.plexpiry.find_tv_seasons(TEST_TV_SHOW)
+        self.assertEqual(FAKE_TV_SEASONS, seasons)
+
+    def test_find_tv_episodes(self):
+        episodes = self.plexpiry.find_tv_episodes(TEST_TV_SHOW, TEST_TV_SEASON)
+        self.assertEqual(FAKE_TV_EPISODES, episodes)
+
+    def test_get_tv_episode(self):
+        episode = self.plexpiry.get_tv_episode(TEST_TV_EPISODE)
+        self.assertEqual(FAKE_TV_EPISODE, episode)
+
+    def test_get_movies(self):
+        movies = self.plexpiry.get_movie_tree()
+        self.assertEqual(FAKE_MOVIES, movies)
+
+    def test_get_movie(self):
+        movie = self.plexpiry.get_movie(TEST_MOVIE)
+        self.assertEqual(FAKE_MOVIE, movie)
